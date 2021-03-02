3e74c773c58bdd12543acf2421a125ce4af1880c
Jerome Haas <j.haas@cyberplanet.ch>
2021-03-02 16:51:55 +0100

    Implement: setup styled components and reset all css default browser styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+import  GlobalStyle from '@/styles/GlobalStyle';
+import React, { Fragment } from 'react';
+
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  
+  return (
+    <Fragment>
+    <GlobalStyle />
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
 
 export default App
