6851c906d20adbfac1857bcf053d756d7a55fa29
Jerome Haas <j.haas@cyberplanet.ch>
2021-03-05 00:18:14 +0100

    Implement: set attribute to make the sections optionally 100vh

--- components/layout/Section.tsx
+++ components/layout/Section.tsx
@@ -4,18 +4,20 @@ import styled from 'styled-components';
 
 interface Props {
   children: React.ReactNode;
+  hero?: Boolean;
 }
 
 const StyledSection = styled.div`
   width: 100%;
+  min-height: ${p => p.hero ? 'calc(100vh - 330px)' : 'unset'};
   margin-bottom: 150px;
 `;
 
-const Section: React.FunctionComponent<Props> = ({ children }) => {
+const Section: React.FunctionComponent<Props> = ({ children, hero }) => {
 
   return (
     <Limiter>
-      <StyledSection>
+      <StyledSection hero>
         {children}
       </StyledSection>
     </Limiter>
