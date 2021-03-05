a143efb39aa976fc018fa39acd74341ea0936666
Jerome Haas <j.haas@cyberplanet.ch>
2021-03-05 11:26:37 +0100

    Refactor: simplify the main text block by only passing one prop with all informations

--- components/sections/Start.tsx
+++ components/sections/Start.tsx
@@ -8,11 +8,22 @@ import Button from '@/components/buttons/Button';
 
 interface Props { }
 
+const content = {
+  section: 'start',
+  title: <>hier beginnt <br />die </>,
+  carouselItems: ['Magie', 'Idee', 'Zusammenarbeit'],
+  text: <Paragraph>reiheacht ist eine digitale Kreativagentur, die mit Innovation und Zusammenarbeit einzigartige Lösungen anbietet, damit du deine Marke da stärken kannst, wo du deine Kunden am besten erreichst. <br /> Wir bewegen alles und jeden – setz dich in die reiheacht.</Paragraph>,
+  buttons: [
+    { content: 'Sitzplatz buchen', link: '#', delay: '5s' },
+    { content: 'Projekte', link: '#', delay: '5.25s' }
+  ]
+}
+
 const Start: React.FunctionComponent<Props> = () => {
 
   return (
     <Section hero={true}>
-      <MainTextBlock section={'start'} buttons={[{ content: 'Sitzplatz buchen', link: '#', delay: '5s' }, { content: 'Projekte', link: '#', delay: '5.25s' }]} />
+      <MainTextBlock content={content} />
     </Section>
   );
 
