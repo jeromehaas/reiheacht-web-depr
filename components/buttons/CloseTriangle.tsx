0bd6f8b929a044d17955f127bc36e332ccee168a
Jerome Haas <j.haas@cyberplanet.ch>
2021-03-06 16:35:28 +0100

    Implement: add link to close triangle

--- components/buttons/CloseTriangle.tsx
+++ components/buttons/CloseTriangle.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Lottie } from '@crello/react-lottie';
 import closeTriangleIn from '../../public/animations/close-triangle-in.json';
 import styled, { css } from 'styled-components';
+import Link from 'next/link';
 
 
 const Wrapper = styled.i`
@@ -37,15 +38,18 @@ const CloseTriangle: React.FunctionComponent<Props> = ({ position }) => {
 
   return (
     <Wrapper direction={position}>
-
+      <Link href='/'>
+        <a>
       <Lottie
-        config={{ animationData: closeTriangleIn, autoplay: true }}
-        playingState={'playing'}
-        speed={1}
-        width={'100px'}
-        height={'100px'}
-        style={{ margin: '0 auto' }}
+      config={{ animationData: closeTriangleIn, autoplay: true }}
+      playingState={'playing'}
+      speed={1}
+      width={'100px'}
+      height={'100px'}
+      style={{ margin: '0 auto' }}
       />
+        </a>
+      </Link>
     </Wrapper>
   );
 
