79ee891a5a13657d7ba769aad2b454c06cad39dc
Jerome Haas <j.haas@cyberplanet.ch>
2021-03-07 13:12:06 +0100

    Refactor: make carousel animation responsive

--- components/animations/Carousel.tsx
+++ components/animations/Carousel.tsx
@@ -7,27 +7,19 @@ const StyledCarousel = styled.span`
   color: ${p => p.theme.blue};
   display: inline-block;
   position: relative;
-  height:  80px;
+  height:  1em;
   width: calc(100% - 130px);
   overflow: hidden;
   vertical-align: middle;
 
-  @media (max-width: 1000px) {
-    width: calc(100% - 95px);;
+  @media (max-width: 800px) {
+    width: calc(100% - 115px);;
   }
 
   @media (max-width: 600px) {
     width: calc(100% - 0px);
   }
 
-  @media (max-width: 550px) {
-    width: calc(100% - 70px);
-  }
-
-  @media (max-width: 450px) {
-    width: calc(100% - 0px);
-    height:  80px;
-  }
 
 `;
 
@@ -35,37 +27,54 @@ const CarouselWrapper = styled.div`
   display: flex;
   flex-direction: column;
   position: absolute;
-  top: 0px;
+  top: -5px;
 
   &.active {
   animation: 2s ease-in-out 2s 1 forwards carousel;
   }
 
   @keyframes carousel {
-      0% {top: -0px;}
-      35% {top: -95px;}
-      65% {top: -95px;}
-      100% {top: -193px;}
+      0% {top: -5px;}
+      35% {top: -100px;}
+      65% {top: -100px;}
+      100% {top: -195px;}
     }
 
-    @media (max-width: 1000px) {
-      top: 10px;
+    @media (max-width: 800px) {
       @keyframes carousel {
-      0% {top: 10px;}
+      0% {top: -5px;}
+      35% {top: -85px;}
+      65% {top: -85px;}
+      100% {top: -165px;}
+      }
+    }
+    
+    @media (max-width: 700px) {
+      @keyframes carousel {
+      0% {top: -5px;}
+      35% {top: -75px;}
+      65% {top: -75px;}
+      100% {top: -145px;}
+      }
+    }
+
+    @media (max-width: 500px) {
+      @keyframes carousel {
+      0% {top: -5px;}
       35% {top: -65px;}
       65% {top: -65px;}
-      100% {top: -140px;}
+      100% {top: -125px;}
+    }
     }
 
-    @media (max-width: 550px) {
-      top: 21px;
+    @media (max-width: 400px) {
       @keyframes carousel {
-      0% {top: 21px;}
-      35% {top: -34px;}
-      65% {top: -34px;}
-      100% {top: -90px;}
+      0% {top: -5px;}
+      35% {top: -55px;}
+      65% {top: -55px;}
+      100% {top: -105px;}
     }
- }
+    }
 
 `;
 
