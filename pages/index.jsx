5c03e5249992edc46ed160a85893b3b9841d0fcc
Jerome Haas <j.haas@cyberplanet.ch>
2021-03-12 23:47:39 +0100

    Experimental: try to find way to use vimeo for the intro animation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import Head from 'next/head';
 import PageWrapper from '@/components/layout/PageWrapper';
 import Navigation from '@/components/partials/Navigation';
@@ -11,7 +11,17 @@ import Footer from '@/components/partials/Footer';
 import Paragraph from '@/components/text/Paragraph';
 import Button from '@/components/buttons/Button';
 import { Lottie } from '@crello/react-lottie';
+import Video from '@/components/partials/Video';
+import ReactPlayer from 'react-player';
+import styled from 'styled-components';
+import Player from '@vimeo/player';
+import $ from 'jquery';
 import introAnimation from '../public/animations/intro.json';
+
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
 
 function useLocalStorage(defaultValue, key) {
   const [value, setValue] = useState(defaultValue);
@@ -30,26 +40,27 @@ function useLocalStorage(defaultValue, key) {
   return [value, setValue];
 }
 
+const PlayerWrapper = styled.div`
+  position: relative;
+  padding-top: 56.25%;
+
+  .react-player {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`;
+
 const Home = () => {
   const [animationPlayed, setAnimationPlayed] = useLocalStorage('true', 'animationWasPlayed');
-
+  const [videoStatus, setVideoStatus] = useState(false);
   return (
     <div>
 
       <PageWrapper>
 
         {animationPlayed !== true ? (
-          <Lottie
-            config={{ animationData: introAnimation, loop: false, autoplay: true }}
-            playingState="playing"
-            speed={1}
-            width="100%"
-            height="100vh"
-            direction={1}
-            lottieEventListeners={[
-              { callback: () => setAnimationPlayed(true), name: 'complete' },
-            ]}
-          />
+          <iframe src="https://player.vimeo.com/video/520902562?api=1&amp;background=1&amp;mute=0&amp;loop=0&amp;autoplay=1" frameBorder="0" id="video" title="Die Antwort Intro Showreel" webkitallowfullscreen="" mozallowfullscreen="" allowFullScreen="allowfullscreen" className="video visible" data-ready="true" />
         ) : (
           <>
             <Navigation />
@@ -60,6 +71,7 @@ const Home = () => {
             <Contact />
             <Footer />
           </>
+
         )}
       </PageWrapper>
     </div>
