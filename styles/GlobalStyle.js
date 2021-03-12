00c738eb0074cc1a87418f2ea645f214a7ea447c
Jerome Haas <j.haas@cyberplanet.ch>
2021-03-12 15:40:02 +0100

    Implement: add new families to project and import the font-face

--- styles/GlobalStyle.js
+++ styles/GlobalStyle.js
@@ -7,7 +7,8 @@ const GlobalStyle = createGlobalStyle`
     outline: none;
     -ms-overflow-style: none;
     scrollbar-width: none;
-    font-family: 'Helvetica 65 Medium', serif;
+    font-family: 'Helvetica Neue 55 Roman', serif;
+    
     .noSelect {
     -webkit-touch-callout: none;
     -webkit-user-select: none;
@@ -18,7 +19,9 @@ const GlobalStyle = createGlobalStyle`
     -webkit-user-drag: none;
     -webkit-user-select: none;
     -webkit-user-drag: none;
-  -webkit-user-select: none;
+    -webkit-user-select: none;
+    }
+
 }
 
 img {
@@ -89,7 +92,7 @@ table {
   border-spacing: 0;
 }
 
-@font-face {
+/* @font-face {
     font-family: 'Helvetica 55 Roman';
     src: local('fonts/helvetica-neue'), url('../fonts/helvetica-neue/HelveticaNeueLTStd-Md.eot?#iefix') format('embedded-opentype'),
         local('fonts/helvetica-neue'), url('../fonts/helvetica-neue/HelveticaNeueLTStd-Md.woff2') format('woff2'),
@@ -111,7 +114,48 @@ table {
     font-weight: normal;
     font-style: normal;
     font-display: swap;
-}
+} */
+
+   
+    @font-face{
+        font-family:"Helvetica Neue 55 Roman";
+        src:
+          url("../fonts/5664093/08b57253-2e0d-4c12-9c57-107f6c67bc49.woff2") format("woff2"),
+          url("../fonts/5664093/08edde9d-c27b-4731-a27f-d6cd9b01cd06.woff") format("woff");
+    }
+    @font-face{
+        font-family:"Helvetica Neue 65 Medium";
+        src:
+          url("../fonts/5664103/240c57a0-fdce-440d-9ce3-85e0cb56f470.woff2") format("woff2"),
+          url("../fonts/5664103/7802e576-2ffa-4f22-a409-534355fbea79.woff") format("woff");
+    }
+    @font-face{
+        font-family:"Helvetica Neue 76 Bold Italic";
+        src:
+          url("../fonts/5664111/13ab58b4-b5ba-4c95-afde-ab2608fbbbd9.woff2") format("woff2"),
+          url("../fonts/5664111/5018b5b5-c821-4653-bc74-d0b11d735f1a.woff") format("woff");
+    }
+    @font-face{
+        font-family:"Helvetica Neue 85 Heavy";
+        src:
+          url("../fonts/5664115/7e42a406-9133-48c0-a705-4264ac520b43.woff2") format("woff2"),
+          url("../fonts/5664115/837750e9-3227-455d-a04e-dc76764aefcf.woff") format("woff");
+    }
+    @font-face{
+        font-family:"Helvetica Neue 95 Black";
+        src:
+          url("../fonts/5664121/fc4fb6ca-f981-4115-b882-c78e9f08be52.woff2") format("woff2"),
+          url("../fonts/5664121/6ed03453-f512-45ba-84bf-fe4ea45d5e6a.woff") format("woff");
+    }
+    @font-face{
+        font-family:"Helvetica Neue 75 Bold";
+        src:
+          url("../fonts/5664150/800da3b0-675f-465f-892d-d76cecbdd5b1.woff2") format("woff2"),
+          url("../fonts/5664150/7b415a05-784a-4a4c-8c94-67e9288312f5.woff") format("woff");
+    }
+
+
+
 
 `;
 
