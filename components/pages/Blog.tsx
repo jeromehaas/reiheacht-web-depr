dadd706aad50685003034cb4e15a83bcd47e8832
Jerome Haas <j.haas@cyberplanet.ch>
2021-03-15 16:36:28 +0100

    Cleanup: pages components

--- components/pages/Blog.tsx
+++ components/pages/Blog.tsx
@@ -2,23 +2,18 @@ import React from 'react';
 import PageWrapper from '@/components/layout/PageWrapper';
 import Section from '@/components/layout/Section';
 import CloseTriangle from '@/components/buttons/CloseTriangle';
-import HorizontalLine from '@/components/shapes/HorizontalLine';
-import ProjectOverview from '@/components/partials/ProjectOverview';
-import Spacer from '@/components/layout/Spacer';
-import Head from 'next/head';
-import Reveal, { Fade } from 'react-awesome-reveal';
+import Space from '@/components/layout/Space';
 
 interface Props {
-  children?: React.ReactNode;
-  content?: any;
-  closeTriangleTarget?: string;
+  closeTriangleTarget: string;
+  children: React.ReactNode;
 }
 
 const Blog: React.FunctionComponent<Props> = ({ children, closeTriangleTarget }) => (
   <div>
     <PageWrapper color="white">
       <CloseTriangle position="top" color="blue" target={closeTriangleTarget} />
-      <Spacer marginBottom="30px" />
+      <Space height="30px" />
       <Section>
         {children}
       </Section>
