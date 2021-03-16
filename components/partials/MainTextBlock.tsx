5610014fe220e9456498dc27ea376c83403dbfca
Jerome Haas <j.haas@cyberplanet.ch>
2021-03-16 15:17:56 +0100

    Fix: make the main text block appearing indipendent of the viewport

--- components/partials/MainTextBlock.tsx
+++ components/partials/MainTextBlock.tsx
@@ -7,10 +7,16 @@ import Carousel from '@/components/animations/Carousel';
 import Reveal from 'react-awesome-reveal';
 import { keyframes } from '@emotion/react';
 import Space from '@/components/layout/Space';
+import styled from 'styled-components';
 
-const moveDown = keyframes`
-  from {top: -60px; opacity: 0.1; position: relative;}
-  to {top: 0px; opacity: 1; position: relative;}
+const MoveDownWrapper = styled.div`
+  animation: slideDown  1s forwards;
+  position: relative;
+
+  @keyframes slideDown {
+    0% {top: -60px; opacity: 0.1; }
+    100% {top: 0px; opacity: 1; }
+  }
 `;
 
 interface Props {
@@ -23,7 +29,7 @@ interface Props {
 }
 
 const MainTextBlock: React.FunctionComponent<Props> = ({ content }) => (
-  <Reveal keyframes={moveDown} triggerOnce fraction={1}>
+  <MoveDownWrapper>
     <H1>{content.title}<Carousel items={content.carouselItems} /></H1>
     <HorizontalLine delay="3.5s" animated />
     <P size="big" animated delay="3.5s">{content.text}</P>
@@ -31,7 +37,7 @@ const MainTextBlock: React.FunctionComponent<Props> = ({ content }) => (
     {content.buttons.map((button) => (
       <Button key={button.text} type="anchor" target={button.target} text={button.text} animate delay={button.delay} />
     ))}
-  </Reveal>
+  </MoveDownWrapper>
 );
 
 export default MainTextBlock;
