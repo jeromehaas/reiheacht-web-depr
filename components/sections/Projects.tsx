554389a22900054dddf78e076d6c218dbb9a0ec4
Jerome Haas <j.haas@cyberplanet.ch>
2021-03-19 13:38:26 +0100

    Fix: change title of one project tile

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -24,7 +24,7 @@ const tileContent = [
     image: '/illustrations/projects/osterkampagne.png',
     alt: 'Gallo Suisse',
     title: 'Gallo Suisse',
-    description: 'Osterkampagne',
+    description: 'Ostern 2021',
     overlayColor: '#C70103',
     link: '/projekte/osterkampagne',
   },
