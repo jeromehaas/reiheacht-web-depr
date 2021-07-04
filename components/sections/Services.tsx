5dfa3aed16946afbe18752a50c8dd9cf2cbf0516
Jerome Haas <hello@jeromehaas.dev>
2021-07-04 20:47:17 +0200

    Revert "Refactor: use new images for the service tiles"
    This reverts commit 4710a2a9572ae93b3e8c2b54ba9f2d1a2c326531.

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -8,42 +8,42 @@ interface Props { }
 
 const tileContent = [
   {
-    image: '/illustrations/services/idea.jpg',
+    image: '/illustrations/services/idea.svg',
     alt: 'Videokonzeption',
     title: 'Videokonzeption',
     overlayColor: 'white',
     link: 'leistungen/videokonzeption',
   },
   {
-    image: '/illustrations/services/storytelling.jpg',
+    image: '/illustrations/services/storytelling.svg',
     alt: 'Storytelling',
     title: 'Storytelling',
     overlayColor: 'orange',
     link: '/leistungen/storytelling',
   },
   {
-    image: '/illustrations/services/illustration.jpg',
+    image: '/illustrations/services/illustration.svg',
     alt: 'Motion Design',
     title: 'Motion Design',
     overlayColor: 'white',
     link: '/leistungen/motion-design',
   },
   {
-    image: '/illustrations/services/film.jpg',
+    image: '/illustrations/services/film.svg',
     alt: 'Erklär Video',
     title: 'Erklärvideo',
     overlayColor: 'orange',
     link: '/leistungen/erklaervideo',
   },
   {
-    image: '/illustrations/services/web.jpg',
+    image: '/illustrations/services/web.svg',
     alt: 'Werbe- & Imagefilm',
     title: 'Werbe- & Imagefilm',
     overlayColor: 'white',
     link: '/leistungen/werbe-und-imagefilm',
   },
   {
-    image: '/illustrations/services/sound.jpg',
+    image: '/illustrations/services/sound.svg',
     alt: 'Sound-Design',
     title: 'Sound-Design',
     overlayColor: 'orange',
