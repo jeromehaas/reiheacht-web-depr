1b7060d383215144a67f09ede58d9978e8c78355
Jerome Haas <hello@jeromehaas.dev>
2021-07-10 22:19:16 +0200

    Refactor: apply new style for service tiles

--- components/tiles/ServiceTile.tsx
+++ components/tiles/ServiceTile.tsx
@@ -37,13 +37,13 @@ const StyledServiceTile = styled.div`
 
   .overlay {
     position: absolute;
-    top: 100%;
+    top: calc(100% - 60px);
     right: 0;
     bottom: 0;
     left: 0;
     width: 100%;
-    height: 100%;
-    padding: 30px;
+    height: 60px;
+    padding: 0 15px;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -53,93 +53,56 @@ const StyledServiceTile = styled.div`
 
   .textWrapper  {
     line-height: 1;
-    opacity: 0;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
     transition: all 0.99s ease-in-out;
   }
 
   ${H3} {
-    top: 30px;
-    font-size: 30px;
-    opacity: 0;
-    position: relative;
-    transition: all 0.7s ease-in;
+    font-size: 25px;
     font-family: 'Helvetica Neue 75 Bold'
   }
 
-  ${P} {
-    top: 30px;
-    opacity: 0;
-    font-size: 25px;
-    position: relative;
-    transition: all 0.7s ease 0.2s;
-    color: ${(p) => p.theme.blue}
+
+  .serviceArrow {
+		width: auto;
+		height: 20px;
+		margin-left: 15px;
   }
 
-  .moreInfoLink {
-    position: absolute;
-    bottom: 30px;
-    font-size: 25px;
-    right: 30px;
-    opacity: 0;
-    font-family: 'Helvetica Neue 55 Roman';
-    color: ${(p) => p.theme.white};
-    text-decoration: underline;
-    transition: all 0.7s ease 0.2s;
-  }
 
-  &:hover {
 
-    .overlay {
-      top: 0px;
-      height: 100%;
-    }
-
-    .textWrapper {
-      margin-top: -30%;
-      opacity: 1;
-    }
-
-    ${H3} {
-      top: 5px;
-      opacity: 1;
-    }
-
-    ${P} {
-      top: 5px;
-      opacity: 1;
-  }
-
-  .moreInfoLink {
-    opacity: 1;
-  }
-
-}
 
 @media (max-width: 1000px) {
     ${H3} { font-size: 25px; };
-    ${P} { font-size: 20px;}
-    .moreInfoLink { font-size: 20px; }
   }
 
   @media (max-width: 850px) {
     width: calc(100% / 2);
-    ${H3} { font-size: 30px; };
-    ${P} { font-size: 25px;
-    .moreInfoLink { font-size: 25px; }
+
   }
 
   @media (max-width: 700px) {
-    ${H3} { font-size: 25px; };
-    ${P} { font-size: 20px;}
-    .moreInfoLink { font-size: 20px; }
   }
 
-  @media (max-width: 550px) {
+  @media (max-width: 580px) {
+		.serviceArrow {
+			width: auto;
+			height: 15px;
+			margin-left: 10px;
+  	}
+  }
+
+	@media (max-width: 550px) {
     width: calc(100% / 1);
     margin-top: 30px;
-    .overlay { height: 60px; }
-    .textWrapper { height: 60px;}
-    .title { height: 60px; }
+		.serviceArrow {
+			width: auto;
+			height: 20px;
+			margin-left: 15px;
+  	}
   }
 
   @media (max-width: 450px) {
@@ -163,16 +126,15 @@ interface Props {
 
 const ServiceTile: React.FunctionComponent<Props> = ({ content }) => (
   <StyledServiceTile>
-    <div className="initial">
-      <Image src={content.image} alt={content.alt} />
-    </div>
     <Link href={content.link}>
       <a>
+        <div className="initial">
+          <Image src={content.image} alt={content.alt} />
+        </div>
         <div className="overlay" style={{ backgroundColor: content.overlayColor === 'orange' ? '#C23E27' : '#FFFFFF' }}>
           <div className="textWrapper">
             <H3 color={content.overlayColor === 'orange' ? 'white' : 'darkGrey'}>{content.title}</H3>
-            <Space height="6px" />
-            <p className="moreInfoLink" style={{ color: `${content.overlayColor === 'orange' ? 'white' : '#1C1C1C'}` }}>Mehr Infos</p>
+            <img className="serviceArrow" src={`${content.overlayColor === 'orange' ? '/icons/arrows/double-arrow-right-white.svg' : '/icons/arrows/double-arrow-right-black.svg'}`} alt="E-Mail Icon" />
           </div>
         </div>
       </a>
