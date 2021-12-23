613e5167ed8f24dac848b298deab9f76f0c96195
Jerome Haas <hello@jeromehaas.dev>
2021-12-23 09:54:08 +0100

    Refactor: remove video and change text for 'it-point' project

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "602e72aaca06d9620d57";
+/******/ 	var hotCurrentHash = "695f63034f854fd0ee22";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/projekte/it-point-imagefilm.tsx
+++ pages/projekte/it-point-imagefilm.tsx
@@ -11,7 +11,7 @@ interface Props { }
 const content = {
   intro: {
     title: <>Empowering People to do what they love<HL color="blue">.</HL></>,
-    text: <>Die ITpoint hat uns damit beauftragt ihr Mission-Statement mit einem Imagefilm in Bewegung zu bringen. “Empowering people to do what they love” steht dabei für die ITpoint im Fokus. Mit den IT-Lösungen ist die ITpoint ein essentielles Zahnrad in vielen Unternehmen, damit diese ihrer Leidenschaft nachgehen können. Ob Bäcker, Chauffeuer, Laborfach- oder Kauffrau, die Lösungen der ITpoint kennen keine Grenzen.</>,
+    text: <>Die ITpoint hat uns damit beauftragt ihr Mission-Statement mit einem Imagefilm in Bewegung zu bringen. “Empowering people to do what they love” steht dabei für die ITpoint im Fokus. Mit ihren IT-Lösungen ist die ITpoint ein essentielles Zahnrad für viele Unternehmen, damit diese ihrer Leidenschaft nachgehen können. Ob Bäcker, Chauffeur, Laborfach- oder Kauffrau, die Lösungen der ITpoint kennen keine Grenzen. Und passend zum internationalen Auftritt der ITpoint gibt es das Video auch mit englischem Voice Over.</>,
   },
   projectOverview: {
     imageLink: '/logos/testimonials/it-point.svg',
@@ -32,13 +32,8 @@ const content = {
 const AgrarInitiative: React.FunctionComponent<Props> = () => (
   <Project content={content}>
     <Head title="Imagefilm ITpoint | reiheacht" description="Empowering people to do what they love, Imagefilm ITpoint" />
-    <H3>Deusche Version:</H3>
     <Space height="15px" />
     <Video link="https://player.vimeo.com/video/659304521?h=a6712c882a" />
-    <Space height="30px" />
-    <H3>Englische Version</H3>
-    <Space height="15px" />
-    <Video link="https://player.vimeo.com/video/659304695?h=3d65df8cd6" />
   </Project>
 );
 
